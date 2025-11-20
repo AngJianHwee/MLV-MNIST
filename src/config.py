c5bb650b3c4b3c614cd0b6e420b4e6b48c4ed3dd
Ang Jian Hwee <angjianhwee@gmail.com>
2025-11-20 17:05:41 +0800

    Update visualization frequency to save frames every 20 iterations

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ BATCH_SIZE = 128
 EPOCHS = 15
 LEARNING_RATE = 0.001
 # How often to save a frame (in iterations)
-VISUALIZATION_FREQUENCY = 1
+VISUALIZATION_FREQUENCY = 20
 
 # Set device
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
